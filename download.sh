50229aa5dc68a0b8d1a9b36f1d5c4a98a5fb3482
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-10-29 22:18:40 +0200

    ...

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.153.159&CultureCode=en" \
+| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > raw_reserves.csv
+
+curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
+| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > raw_discovery_overview.csv
+
+curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_production_monthly&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
+| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > raw_production_monthly_field.csv
+
