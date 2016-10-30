19f92f2ada2564809a888f3f2e13e1a0c7af2496
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-10-30 20:31:23 +0100

    Add screenshots to build

--- make_all.sh
+++ make_all.sh
@@ -5,3 +5,13 @@ set -ex
 ./download.sh
 ./generate.py
 ./make_readme.sh
+
+# create those screenshots....
+rm -rf img/
+mkdir img
+./node_modules/.bin/webpack-dev-server ./vis/entry --hot --inline --module-bind "css=style\!css" &
+SERVER_PID=$!
+sleep 5 # such hack...
+echo "starting to take screenshots ..."
+./screenshots.py
+kill $SERVER_PID
