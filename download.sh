da47addfab3ff40c295e92e5cdf89d1266eba93b
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-11-19 22:05:28 +0100

    Remove CR, Add 33/9-6 DELTA to raw_reserves_field

--- download.sh
+++ download.sh
@@ -5,10 +5,16 @@ set -ex
 mkdir -p data/
 
 curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
+| tr -d '\r' \
 | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > data/raw_discovery_overview.csv
 
 curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_production_monthly&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
+| tr -d '\r' \
 | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > data/raw_production_monthly_field.csv
 
 curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
-| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > data/raw_reserves_field.csv
+| tr -d '\r' \
+| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' \
+| awk 'NF' > data/raw_reserves_field.csv
+diff <(head -n 1 data/raw_reserves_field.csv) <(head -n 1 extra_data/field_reserves_extra.csv)
+awk 'NF' extra_data/field_reserves_extra.csv | tail -n +2 >> data/raw_reserves_field.csv
