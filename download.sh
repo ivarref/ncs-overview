6ab974f83e9d518ff1d5c9817170cbf9880ff027
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-11-19 21:36:48 +0100

    Add reserve data

--- download.sh
+++ download.sh
@@ -9,3 +9,6 @@ curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery&rs:Com
 
 curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_production_monthly&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
 | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > data/raw_production_monthly_field.csv
+
+curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
+| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' > data/raw_reserves_field.csv
