dd84ebe2818d18965d06bdf3d38b1e3b51069eb8
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-11-20 11:35:33 +0100

    Add 33/9-6 DELTA at front of raw_reserves_field

--- download.sh
+++ download.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-mkdir -p data/
+rm -rf data/
+rm -rf tmp_data/
+mkdir data/
+mkdir tmp_data/
 
 curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
 | tr -d '\r' \
@@ -19,6 +22,7 @@ curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_production
 curl "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.255.240&CultureCode=en" \
 | tr -d '\r' \
 | awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' \
-| awk 'NF' > data/raw_reserves_field.csv
-diff <(head -n 1 data/raw_reserves_field.csv) <(head -n 1 extra_data/field_reserves_extra.csv)
-awk 'NF' extra_data/field_reserves_extra.csv | tail -n +2 >> data/raw_reserves_field.csv
+| awk 'NF' > tmp_data/raw_reserves_field.csv
+diff <(head -n 1 tmp_data/raw_reserves_field.csv) <(head -n 1 extra_data/field_reserves_extra.csv)
+awk 'NF' extra_data/field_reserves_extra.csv > data/raw_reserves_field.csv
+cat tmp_data/raw_reserves_field.csv | tail -n +2 >> data/raw_reserves_field.csv
