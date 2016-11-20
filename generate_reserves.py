017935cf987a36181ac2dcbe114845e984e2f05b
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-11-20 12:09:01 +0100

    Add reserves_percentage_by_decade

--- generate_reserves.py
+++ generate_reserves.py
@@ -33,26 +33,46 @@ if __name__=="__main__":
   decade_to_fields = generate.get_decade_to_fields(id_to_year)
   print("reserves")
   print("-"*80)
+  
+  def sum_entry(decade, fields, mod_fn=lambda prop, v: v):
+    def sum_prop(destination_prop, prop):
+        return (destination_prop, mod_fn(destination_prop, sum([remaining_reserve(field_id, prop) for field_id in fields])))
+    d = collections.OrderedDict([
+        ('name', str(decade)),
+        sum_prop('origRecoverableOil', 'fldRecoverableOil'),
+        sum_prop('origRecoverableGas', 'fldRecoverableGas'),
+        sum_prop('origRecoverableOE', 'fldRecoverableOE'),
+        sum_prop('remainingOil', 'fldRemainingOil'),
+        sum_prop('remainingGas', 'fldRemainingGas'),
+        sum_prop('remainingOE', 'fldRemainingOE')])
+    return d
+  
   with codecs.open('./data/reserves_OEMillSm3_by_decade.csv', mode='w', encoding='utf8') as wfd:
+    def write(line):
+      wfd.write(line)
+      wfd.write("\n")
+      print(line)
     decades = decade_to_fields.items()
     decades.append(('Sum', ids))
     for (idx, (decade, fields)) in enumerate(decades):
-      def write(line):
-        wfd.write(line)
-        wfd.write("\n")
-        print(line)
-      
-      def sum_prop(prop):
-        return sum([remaining_reserve(field_id, prop) for field_id in fields])
+      d = sum_entry(decade, fields)
+      if (idx == 0):
+        write(",".join(d.keys()))
+      write(",".join([str(x) for x in d.values()]))
+  print("-"*80)
 
-      d = collections.OrderedDict([
-          ('name', str(decade)),
-          ('origRecoverableOil', sum_prop('fldRecoverableOil')),
-          ('origRecoverableGas', sum_prop('fldRecoverableGas')),
-          ('origRecoverableOE', sum_prop('fldRecoverableOE')),
-          ('remainingOil', sum_prop('fldRemainingOil')),
-          ('remainingGas', sum_prop('fldRemainingGas')),
-          ('remainingOE', sum_prop('fldRemainingOE'))])
+  with codecs.open('./data/reserves_percentage_by_decade.csv', mode='w', encoding='utf8') as wfd:
+    def write(line):
+      wfd.write(line)
+      wfd.write("\n")
+      print(line)
+    decades = decade_to_fields.items()
+    decades.append(('Sum', ids))
+    o = sum_entry('Sum', ids)
+    for (idx, (decade, fields)) in enumerate(decades):
+      def relative(prop, v):
+        return "%.2f" % ((100.0*v) / o[prop])
+      d = sum_entry(decade, fields, relative)
       if (idx == 0):
         write(",".join(d.keys()))
       write(",".join([str(x) for x in d.values()]))
