9336870e91561eb2f8d2b21e43ef826161efdcd9
Ivar Refsdal <ivar.refsdal@nsd.no>
2016-12-02 19:41:14 +0100

    Try to crop those images properly

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 clear
-./generate.py
+curl -H "Content-Type: application/json" -d '{"url":"localhost:8080/bundle?mode=oil", "clipRect": "0,0,642,335", "force": true, "width": 10, "height": 10}' http://localhost:8891/ > ./img/oil_production_yearly_12MMA_by_discovery_decade.png
+#./generate.py
