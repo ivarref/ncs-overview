3de450867f2c33f8a283800d8233e6c273ca5e5a
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-12-03 12:32:36 +0100

    A fair bit accomplished on #10

--- vis/cumulative_produced_and_reserves.js
+++ vis/cumulative_produced_and_reserves.js
@@ -14,7 +14,8 @@ function show_resource(unit_key, resource, group, unit, file) {
   var svg = d3.select('body').append('svg')
     .attr('width', 960 / 1.5)
     .attr('height', 500 / 1.5)
-    .style('border', "1px solid #000000");
+    //.style('border', "1px solid #000000")
+
 
   var margin = { top: 40, right: 20, bottom: 30, left: 50 },
     width = +svg.attr("width") - margin.left - margin.right,
@@ -23,62 +24,82 @@ function show_resource(unit_key, resource, group, unit, file) {
 
   d3.csv(file,
     function (d) {
-      return { 
-        'name' : d['name'],
-        'origRecoverable': +d['origRecoverable' + unit_key], 
+      return {
+        'name': d['name'],
+        'origRecoverable': +d['origRecoverable' + unit_key],
         'produced': +d['origRecoverable' + unit_key] - +d['remaining' + unit_key],
         'remaining': +d['remaining' + unit_key],
       };
     },
     function (error, data) {
       if (error) throw error;
-      data = data.filter(function(d) { return d.name !== 'Sum'; });
-      var keys = data.map(function(d) { return d.name });
-      console.log("keys is", keys);
+      data = data.filter(function (d) { return d.name !== 'Sum'; });
+      var keys = data.map(function (d) { return d.name });
 
       var y = d3.scaleLinear()
-        .domain([0, d3.max(data.map(function(d) { return d.origRecoverable }))])
+        .domain([0, d3.max(data.map(function (d) { return d.origRecoverable }))])
         .range([height, 0]);
+
+      var x = d3.scaleBand()
+        .domain(data.map(function (d) { return d.name }))
+        .padding(0.1)
+        .rangeRound([0, width])
 
       var colorscheme = group === 'funntiår' ? d3.schemeCategory10 : ["#bcbd22", "#17becf", "#e377c2"];
 
-      var bar = svg.selectAll('g')
+      var barWidth = width / data.length;
+
+      var bar = svg.append('g').selectAll('g')
         .data(data)
         .enter()
         .append('g')
-        .attr('transform', function(d, i) { return 'translate(' + i*50 + ',0)'; });
-      
-      bar.append('g')
-      .append('rect')
-      .attr('width', 15)
-      .attr('y', function(d) { return y(d.remaining)} )
-      .attr('height', function(d) { return height - y(d.remaining)} )
-      .style('fill', function(d, i) { return colorscheme[i] })
-
-      bar.append('g')
-      .append('rect')
-      .attr('width', 15)
-      .attr('y', function(d) { return y(d.origRecoverable)} )
-      .attr('height', function(d) { return height - y(d.produced) } )
-      .style('fill', function(d, i) { return colorscheme[i] })
-      .style('fill-opacity', '0.5')
-
-      bar
-        .append('text')
-        .style('text-anchor', 'middle')
-        .attr('dx', 15/2)
-        .text(function(d) { return d.name })
+        .attr('transform', function (d) { return 'translate(' + x(d.name) + ',0)'; });
 
       svg.append("g")
-                .call(d3.axisLeft(y))
-                .append("text")
-                .attr("fill", "#000")
-                .attr("transform", "rotate(-90)")
-                .attr("y", -margin.left + 8)
-                .attr("x", -height / 2)
-                .attr("dy", "0.71em")
-                .style("text-anchor", "middle")
-                .text(unit);
+        .append("text")
+        .attr('dy', '-.35em')
+        .attr('x', width / 2)
+        .style("text-anchor", "middle")
+        .classed("heading", true)
+        .text("Norsk " + resource.toLowerCase() + ": Produsert og reserver etter " + group)
+
+      bar.append('g')
+        .append('rect')
+        .attr('width', x.bandwidth())
+        .attr('y', function (d) { return y(d.remaining) })
+        .attr('height', function (d) { return height - y(d.remaining) })
+        .style('fill', function (d, i) { return colorscheme[i] })
+
+      bar.append('g')
+        .append('rect')
+        .attr('width', x.bandwidth())
+        .attr('y', function (d) { return y(d.origRecoverable) })
+        .attr('height', function (d) { return height - y(d.produced) })
+        .style('fill', function (d, i) { return colorscheme[i] })
+        .style('fill-opacity', '0.45')
+
+      // bar
+      //   .append('text')
+      //   .style('text-anchor', 'middle')
+      //   .attr('x', x.bandwidth() / 2)
+      //   .attr('y', function (d) { return y(d.origRecoverable) + 3 })
+      //   .attr('dy', '.75em')
+      //   .text(function (d) { return d.origRecoverable })
+
+      svg.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 8)
+        .attr("x", -height / 2)
+        .attr("dy", "0.71em")
+        .style("text-anchor", "middle")
+        .text(unit);
+
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
     });
 }
@@ -97,13 +118,36 @@ var m = {
     unit_key: 'Gas',
     group: 'funntiår',
     filename: '/data/decade/reserves_gboe_by_decade.csv'
+  },
+  petroleum: {
+    title: 'Petroleum',
+    unit: 'Milliarder fat oljeekvivalenter',
+    unit_key: 'OE',
+    group: 'funntiår',
+    filename: '/data/decade/reserves_gboe_by_decade.csv'
+  },
+  oil_region: {
+    title: 'Olje',
+    unit: 'Milliarder fat',
+    unit_key: 'Oil',
+    group: 'region',
+    filename: '/data/region/reserves_gboe_by_region.csv'
+  },
+  gas_region: {
+    title: 'Gass',
+    unit: 'Milliarder fat oljeekvivalenter',
+    unit_key: 'Gas',
+    group: 'region',
+    filename: '/data/region/reserves_gboe_by_region.csv'
+  },
+  petroleum_region: {
+    title: 'Petroleum',
+    unit: 'Milliarder fat oljeekvivalenter',
+    unit_key: 'OE',
+    group: 'region',
+    filename: '/data/region/reserves_gboe_by_region.csv'
   }
 };
 
 var mode = findGetParameter('mode', 'oil');
 show_resource(m[mode].unit_key, m[mode].title, m[mode].group, m[mode].unit, m[mode].filename);
-
-// var data = require('../data/decade/reserves_gboe_by_decade.json');
-// console.log('start data...');
-// console.log(data);
-// console.log('end data...');
