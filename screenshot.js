cf2a4659a8f11987ff1291aeabb985f0c0e7a007
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-12-03 11:34:52 +0100

    Remove old screenshot code

--- screenshot.js
+++
@@ -1,9 +0,0 @@
-var page = require('webpage').create();
-var system = require('system');
-address = system.args[1];
-output = system.args[2];
-
-page.open(address, function() {
-  page.render(output);
-  phantom.exit();
-});
