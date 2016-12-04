c3a624dbdef3bbb0e9fe74f1728a32bcae78f773
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-12-04 15:11:50 +0100

    Remove old files

--- build_linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-while find . -type f -not -path "./.git/*" -not -path "./node_modules/*" -not -name "*.pyc" -not -name "*.png" | inotifywait --fromfile - -e close_write; do clear && ./make.sh $@; done
