2edfe53209929a5ea6ce637eca40443e7aa343f4
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-12-10 15:31:41 +0100

    Don't do pandas rounding

--- split_monthly_production.py
+++ split_monthly_production.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-import pandas as pd
+import csv
 import os
+import codecs
+
+import pandas as pd
 
 if __name__=="__main__":
   frame = pd.read_csv('./data/raw_production_monthly_field.csv')
@@ -14,6 +17,19 @@ if __name__=="__main__":
 
   for name in names:
     fil = './data/fields/' + name.replace('/', '%2F') + '.csv'
-    f = frame[frame[u'prfInformationCarrier'] == name]
-    f.to_csv(fil, line_terminator='\n')
-    print("generated", fil)
+    print("writing", fil)
+    with codecs.open('./data/raw_production_monthly_field.csv', mode='rb', encoding='utf8') as fd:
+      reader = csv.reader(fd)
+      with open(fil, mode='w', encoding='utf8') as wfd:
+        writer = csv.writer(wfd, lineterminator='\n')
+        row_to_idx = {}
+        for (row_idx, row) in enumerate(reader):
+          if row_idx == 0:
+            row_to_idx = dict([(k, idx) for (idx, k) in enumerate(row)])
+            writer.writerow([x for x in row])
+          elif row[row_to_idx[u'prfInformationCarrier']] == name:
+            writer.writerow([x for x in row])
+          
+#    f = frame[frame[u'prfInformationCarrier'] == name]
+ #   f.to_csv(fil, line_terminator='\n')
+ #   print("generated", fil)
