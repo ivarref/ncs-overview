617fc110f242dd1e76bae81a1abab6b99a76d19d
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-01-18 19:52:10 +0100

    Add gboe overview

--- add_discovery_year.py
+++ add_discovery_year.py
@@ -12,21 +12,27 @@ if __name__=="__main__":
 
   with open('./data/raw_reserves_field_discovery_year.csv', 'w') as wfd:
     writer = csv.writer(wfd)
+    with open('./data/raw_reserves_field_discovery_year_gboe.csv', 'w') as wfd2:
+      writer_gboe = csv.writer(wfd2)
 
-    with open('./data/raw_reserves_field.csv', 'r') as rfd:
-      reader = csv.reader(rfd)
-      for (idx, row) in enumerate(reader):
-        if idx==0:
-          if row[0] != 'fldName':
-            print("format changed, expected fldName for first column, was", row[0])
-            sys.exit(1)
-          header = [row[0], 'fldDiscoveryYear'] + row[1:]
-          writer.writerow(header)
-        else:
-          fldName = row[0]
-          f = frame[frame['fldName'] == fldName]
-          years = f['dscDiscoveryYear'].unique()
-          if len(years) != 1:
-            print("expected a single year, got", len(years), "for field", fldName)
-            sys.exit(1)
-          writer.writerow([row[0], years[0]] + row[1:])
+      with open('./data/raw_reserves_field.csv', 'r') as rfd:
+        reader = csv.reader(rfd)
+        for (idx, row) in enumerate(reader):
+          if idx==0:
+            if row[0] != 'fldName':
+              print("format changed, expected fldName for first column, was", row[0])
+              sys.exit(1)
+            header = [row[0], 'fldDiscoveryYear'] + row[1:]
+            writer.writerow(header)
+            writer_gboe.writerow(header)
+          else:
+            fldName = row[0]
+            f = frame[frame['fldName'] == fldName]
+            years = f['dscDiscoveryYear'].unique()
+            if len(years) != 1:
+              print("expected a single year, got", len(years), "for field", fldName)
+              sys.exit(1)
+            writer.writerow([row[0], years[0]] + row[1:])
+            def to_gboe(items):
+              return ["%.2g" % ((float(x)*6.29)/1000.0) for x in items]
+            writer_gboe.writerow([row[0], years[0]] + to_gboe(row[1:-1]) + [row[-1]])
