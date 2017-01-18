e627c74d1ea5afcf04d8e665c69c04d6fcacb6be
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-01-18 19:57:54 +0100

    Update data

--- make_all.sh
+++ make_all.sh
@@ -20,6 +20,7 @@ fi
 ./generate.py
 ./generate_reserves.py
 ./add_discovery_year.py
+./drop_columns.py data/raw_reserves_field_discovery_year_mboe.csv fldRecoverableNGL fldRecoverableCondensate fldRemainingNGL fldRemainingCondensate
 ./generate_kumulativ_reservetilvekst_vs_produksjon.py
 ./calc_wells.py
 ./split_monthly_production.py
