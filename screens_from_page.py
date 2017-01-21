e07ebb84a267fb79b79b14057e34e8603829c580
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-01-21 12:19:36 +0100

    Update data and fix #55

--- screens_from_page.py
+++ screens_from_page.py
@@ -5,8 +5,9 @@ from __future__ import print_function
 
 import codecs
 import json
-import sys
 import os
+import sys
+import time
 import urllib
 
 import requests
@@ -14,6 +15,7 @@ import yaml
 from slimit import ast
 from slimit.parser import Parser
 from slimit.visitors import nodevisitor
+
 
 def get_m_decl(fil):
   with codecs.open(fil, 'r', encoding='utf8') as fd:
@@ -31,13 +33,22 @@ if __name__=="__main__":
   js = yaml.load(m)
   self_ip = os.environ.get('self_ip', 'localhost')
   docker_ip = os.environ.get('docker_ip', 'localhost')
-  for (k, v) in js.items():
+  for (k, v) in sorted(js.items()):
     mode = k
-    img = v['screenshot']
-    cmd = """curl -H "Content-Type: application/json" -d '{"url":"%s:8080/bundle?mode=%s", "force": true, "width": 10, "height": 10}' http://%s:8891/ -o ./img/%s""" % (self_ip, mode, docker_ip, img)
+    img = './img/' + v['screenshot']
+    cmd = """curl -H "Content-Type: application/json" -d '{"url":"%s:8080/bundle?mode=%s", "force": true, "width": 10, "height": 10}' http://%s:8891/ -o %s""" % (self_ip, mode, docker_ip, img)
     print(cmd.encode('utf8'))
-    ret = os.system(cmd.encode('utf8'))
-    print("command returned with", ret)
-    if ret != 0:
-      print("failure!")
-      sys.exit(1)
+    def screenshot(retries=0):
+      ret = os.system(cmd.encode('utf8'))
+      print("command returned with", ret)
+      if ret != 0:
+        print("failure!")
+        sys.exit(1)
+      elif os.stat(img).st_size == 2779 and retries <= 5: # all white image!
+        print("retrying image screenshot ...!")
+        time.sleep(1)
+        screenshot(1+retries)
+      elif os.stat(img).st_size == 2779:
+        print("could not generate non-white screenshot for img", img, "!")
+        sys.exit(1)
+    screenshot()
