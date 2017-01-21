bff3f57e0c63287c5701d70ce82e6758194afeae
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-01-21 12:54:04 +0100

    Update data

--- generate_field_percentage_produced.py
+++ generate_field_percentage_produced.py
@@ -46,12 +46,12 @@ if __name__=="__main__":
 
     for f in ['./data/field_since_2000_percentage_produced.csv', './data/field_percentage_produced.csv']:
       frame = pd.read_csv(f)
-      frame.sort_values(by='percentageProducedOE', ascending=False).to_csv(f, index=False)
+      frame.sort_values(by=['percentageProducedOE', 'fldDiscoveryYear', 'fldName'], ascending=[False, True, True]).to_csv(f, index=False)
     
-    f = pd.read_csv('./data/field_percentage_produced.csv').sort_values(by='percentageProducedOil', ascending=False)
+    f = pd.read_csv('./data/field_percentage_produced.csv').sort_values(by=['percentageProducedOil', 'fldDiscoveryYear', 'fldName'], ascending=[False, True, True])
     f = f[f.percentageProducedOil >= 0]
     f.to_csv('./data/field_percentage_produced_oil.csv', index=False)
     
-    f = pd.read_csv('./data/field_percentage_produced.csv').sort_values(by='percentageProducedGas', ascending=False)
+    f = pd.read_csv('./data/field_percentage_produced.csv').sort_values(by=['percentageProducedGas', 'fldDiscoveryYear', 'fldName'], ascending=[False, True, True])
     f = f[f.percentageProducedGas >= 0]
     f.to_csv('./data/field_percentage_produced_gas.csv', index=False)
