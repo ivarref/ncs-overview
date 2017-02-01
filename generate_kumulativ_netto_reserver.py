faf87425c6c2a06ce89d1084881930bfd93427ae
Ivar Refsdal <ivar.refsdal@nsd.no>
2017-02-01 17:11:42 +0100

    Add some data

--- generate_kumulativ_netto_reserver.py
+++ generate_kumulativ_netto_reserver.py
@@ -10,24 +10,29 @@ import generate
 
 
 if __name__=="__main__":
-  production = pd.read_csv('./data/raw_production_yearly_field.csv')
-  reserves = pd.read_csv('./data/raw_reserves_field_discovery_year.csv')
+  def process(reserve_kind, prod_kind):
+    production = pd.read_csv('./data/raw_production_yearly_field.csv')
+    reserves = pd.read_csv('./data/raw_reserves_field_discovery_year.csv')
 
-  start_year = reserves.fldDiscoveryYear.min()
-  stop_year = max(production.prfYear.max(), reserves.fldDiscoveryYear.max())
+    start_year = reserves.fldDiscoveryYear.min()
+    stop_year = max(production.prfYear.max(), reserves.fldDiscoveryYear.max())
+    
+    decade_to_fields = generate.get_decade_to_fields(generate.get_discovery_years(reserves.fldName.unique()))
+    
+    res = []
+    years = range(start_year, stop_year+1)
+    for year in years:
+      r = []
+      for (decade, fields) in decade_to_fields.items():
+        cumulative_reserves = reserves[(reserves.fldDiscoveryYear <= year)  & reserves.fldName.isin(fields)][reserve_kind].sum()
+        cumulative_produced = production[(production.prfYear <= year)  & production.prfInformationCarrier.isin(fields)][prod_kind].sum()
+        mboe = 6.29*(cumulative_reserves - cumulative_produced)
+        r.append(mboe)
+      res.append([year] + r + [sum(r)])
+    frame = pd.DataFrame(res, columns=['year'] + list(decade_to_fields.keys()) + ['Sum'])
+    return frame
   
-  decade_to_fields = generate.get_decade_to_fields(generate.get_discovery_years(reserves.fldName.unique()))
-  
-  res = []
-  years = range(start_year, stop_year+1)
-  for year in years:
-    r = []
-    for (decade, fields) in decade_to_fields.items():
-      cumulative_reserves = reserves[(reserves.fldDiscoveryYear <= year)  & reserves.fldName.isin(fields)]['fldRecoverableOil'].sum()
-      cumulative_produced = production[(production.prfYear <= year)  & production.prfInformationCarrier.isin(fields)]['prfPrdOilNetMillSm3'].sum()
-      mboe = 6.29*(cumulative_reserves - cumulative_produced)
-      r.append(mboe)
-    res.append([year] + r + [sum(r)])
-  frame = pd.DataFrame(res, columns=['year'] + list(decade_to_fields.keys()) + ['Sum'])
-  frame.to_csv('./data/cumulative net reserves oil mboe.csv', index=False, float_format="%.1f")
+  process('fldRecoverableOil', 'prfPrdOilNetMillSm3').to_csv('./data/cumulative net reserves oil mboe.csv', index=False, float_format="%.1f")
+  process('fldRecoverableGas', 'prfPrdGasNetBillSm3').to_csv('./data/cumulative net reserves gas mboe.csv', index=False, float_format="%.1f")
+  process('fldRecoverableOE', 'prfPrdOeNetMillSm3').to_csv('./data/cumulative net reserves oe mboe.csv', index=False, float_format="%.1f")
   
